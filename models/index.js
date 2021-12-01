6b46aceb00426117b6b1ae1f38ffcb0256348013
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-01 03:52:39 -0700

    post model created

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 
 //set up user model and export an object with it as a property
 const User = require('./User');
+const Post = require('./Post');
 module.exports = {User};
---
+++ models/post.js
@@ -0,0 +1,46 @@
+const { Model, Datatypes, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//create and define the post model 
+class Post extends Model {}
+
+//create fields/columns for post model 
+Post.init(
+    {
+        //define the post schema 
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false, 
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            AllowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            //use references to eastablish relationship between this 
+            //post and the user by referencing the user model 
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+    }
+);
+
+module.exports= Post;

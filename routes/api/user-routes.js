6af08a21415c1bed302a8ead19ded3aedd287890
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-07 09:18:29 -0700

    added comment functionallity

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -19,10 +19,22 @@ router.get('/', (req, res) => {
 //GET API/USERS/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id: req.params.id
+        },
        include: [
            {
                model: Post, 
                attributes: ['id', 'title', 'post_url', 'created_at']
+           },
+           {
+               model: Comment,
+               attributes: ['id', 'comment_text', 'created_at'],
+               include: {
+                   model: Post,
+                   attributes: ['title']
+               }
            },
            {
             model: Post, 
